a7256bb3b06bb369d98e25494a7234cb35978126
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-09-27 17:01:17 -0700

    Fix earlier rename

--- test/Main.purs
+++ test/Main.purs
@@ -5,7 +5,7 @@ import Prelude
 import Data.Map (lookup, member) as Map
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
-import RatioFinder (Edge(..), entryNodes)
+import RatioFinder (Edge(..), allNodes)
 import Test.Unit (test)
 import Test.Unit.Assert (assert, equal) as Assert
 import Test.Unit.Main (runTest)
@@ -14,5 +14,5 @@ main :: Effect Unit
 main =
   runTest do
     test "builds the graph" do
-      Assert.assert "foot in map" $ Map.member "foot" entryNodes
-      Assert.equal (Just [ Edge 0.9144 "meter" ]) $ Map.lookup "yard" entryNodes
+      Assert.assert "foot in map" $ Map.member "foot" allNodes
+      Assert.equal (Just [ Edge 0.9144 "meter" ]) $ Map.lookup "yard" allNodes
